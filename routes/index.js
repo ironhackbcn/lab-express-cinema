5d04664cfd827f0f73684eb97ad7af44133896f3
Ester Fernandez <esterfernand95@gmail.com>
2019-03-01 22:02:07 +0100

    created database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie');
+const data = require('../bin/seeds.js');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/ironhack-cinema', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+// Movie.insertMany(data)
+//   .then(result => {
+//     console.log(result);
+//     mongoose.connection.close();
+//   })
+//   .catch(err => console.log(err));
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  res.render('movies');
+});
+
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const tortillas = await Tortilla.find();
+//     res.render('list', { tortillas });
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 module.exports = router;
