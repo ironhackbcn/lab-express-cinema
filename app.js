184b263898e5097cb29f9d87a406546d49dbe181
Ivan <4esoquevedoi@gmail.com>
2019-03-02 11:08:50 +0100

    setup ready

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 // -- 404 and error handler
 
-// NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
   res.status(404);
   res.render('not-found');
