42f4aecee73e08d03144d9d04c4a1566334b3ccd
Ivan <4esoquevedoi@gmail.com>
2019-03-02 15:15:48 +0100

    movies page finished

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const data = require('../bin/seeds');
+const Movies = require('../models/Movie');
 
-/* GET home page. */
+mongoose.connect('mongodb://localhost/cinema', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+// // Insert data first time
+// Movies.insertMany(data)
+//   .then(result => {
+//     console.log('Datta added');
+//   })
+//   .catch(err => console.error(err));
+
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
-// Cambiar para el enlace a las peliculas
-router.get('/movies', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get('/movies', async (req, res, next) => {
+  try {
+    const movies = await Movies.find();
+    res.render('movies', { movies });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/movies/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    console.log(id);
+    const movie = Movies.findById(id);
+    res.render('movie-details', { movie });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
