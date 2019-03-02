526731453302ee963ec47f5bc65c38c124410f1e
danieltellezllinas <danieltellezllinaas@gmail.com>
2019-03-02 20:14:57 +0100

    Iteration3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
+const Movie = require('../models/Movie');
+const data = require('../bin/seeds.js');
+const mongoose = require('mongoose');
+// Movie.insertMany(data)
+//   .then(result => {
+//     console.log(result);
+//     mongoose.connection.close();
+//   })
+//   .catch(err => console.log(err));
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/movies', async (req, res, next) => {
+  try {
+    const movies = await Movie.find();
+    res.render('movies', { movies });
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
