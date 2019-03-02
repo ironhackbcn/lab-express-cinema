a9ff5608df060bb382aa4707087709aaf47d96f8
danieltellezllinas <danieltellezllinaas@gmail.com>
2019-03-02 20:42:02 +0100

    Final

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Movie = require('../models/Movie');
 const data = require('../bin/seeds.js');
 const mongoose = require('mongoose');
+
 // Movie.insertMany(data)
 //   .then(result => {
 //     console.log(result);
@@ -17,6 +18,16 @@ router.get('/movies', async (req, res, next) => {
   try {
     const movies = await Movie.find();
     res.render('movies', { movies });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/movies/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const moviedetails = await Movie.findById(id);
+    res.render('info', moviedetails);
   } catch (error) {
     next(error);
   }
