3f1237075f8df0f2f6595259cac37ca678ae10c8
Miguel Sanchez <migsandia@gmail.com>
2019-03-03 18:41:13 +0100

    se realiza pagina movies.hbs con css

--- models/Movie.js
+++ models/Movie.js
@@ -2,6 +2,15 @@
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+// const seeds = require('../bin/seeds.js')
+
+mongoose.connect('mongodb://localhost/cinema', {
+
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+
+})
 
 const movieSchema = new Schema({
   title: {
@@ -27,4 +36,13 @@ const movieSchema = new Schema({
 
 const Movie = mongoose.model('Movie', movieSchema)
 
+/* Movie.insertMany(seeds)
+  .then(result => {
+    console.log(result)
+    mongoose.connection.close()// Cerrar la conexion con la base de datos
+  })
+  .catch(error => {
+    console.log(error)
+  })
+*/
 module.exports = Movie
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const Movie = require('../models/Movie')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Cinema Ironhack' })
+})
+
+router.get('/movies', async (req, res, next) => {
+  try {
+    const movies = await Movie.find()
+    res.render('movies', { movies })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
