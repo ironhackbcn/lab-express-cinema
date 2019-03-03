c51e0837bf7ba57e1d6247516c9797caba361bbd
stellarwaves <matias.salasdev@gmail.com>
2019-03-04 00:07:38 +0100

    iteración 4

--- app.js
+++ app.js
@@ -1,49 +1,61 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const mongoose = require('mongoose')
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index')
 
-const app = express();
+const app = express()
 
 mongoose.connect('mongodb://localhost/cinema', {
   keepAlive: true,
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE
-});
+})
+
+// const movies = require('./bin/seeds')
+// const Movie = require('./models/Movies')
+// Movie.insertMany(movies)
+//   .then(result => {
+//     console.log(result)
+//     mongoose.connection.close()
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 // -- 404 and error handler
 
 // NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
-  res.status(404);
-  res.render('not-found');
-});
+  res.status(404)
+  res.render('not-found')
+})
 
 // NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
   // always log the error
-  console.error('ERROR', req.method, req.path, err);
+  console.error('ERROR', req.method, req.path, err)
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
-    res.status(500);
-    res.render('error');
+    res.status(500)
+    res.render('error')
   }
-});
+})
 
-module.exports = app;
+module.exports = app
--- bin/seeds.js
+++ bin/seeds.js
@@ -1,66 +1,70 @@
-const  movies  = [
-   {
-     Título :  " Una arruga en el tiempo " ,
-     director :  " Ava DuVernay " ,
-     estrellas : [ " Storm Reid " , " Oprah Winfrey " , " Reese Witherspoon " ],
-     imagen :  " https://images-na.ssl-images-amazon.com/images/M/MV5BMjMxNjQ5MTI3MV5BMl5BanBnXkFtZTgwMjQ2MTAyNDM@._V1_UX182_CR0,0,182,268_AL_.jpg " ,
-     descripción :  " Tras el descubrimiento de una nueva forma de viaje espacial y la desaparición del padre de Meg, ella, su hermano y su amiga deben unirse a tres seres mágicos: la Sra. Whatsit, la Sra. Who y la Sra. Which para viajar el universo de rescatarlo de un terrible mal. " ,
-     horarios : [ " 13:00 " , " 15:30 " , " 18:00 " , " 20:10 " , " 22:40 " ]
-   },
-   {
-     Título :  " The Strangers: Prey at Night " ,
-     Director :  " Johannes Roberts " ,
-     estrellas : [ " Christina Hendricks " , " Bailee Madison " , " Martin Henderson " ],
-     imagen :  " https://images-na.ssl-images-amazon.com/images/M/MV5BMTY1OTIwODgzMV5BMl5BanBnXkFtZTgwMzUyMDgzNDM@._V1_UX182_CR0,082-68_AL_.jpg " ,
-     descripción :  " El viaje por carretera de una familia toma un giro peligroso cuando llegan a un parque de casas móviles aisladas para quedarse con algunos familiares y lo encuentran misteriosamente desierto. Bajo la cobertura de la oscuridad, tres psicópatas enmascarados les visitan para probar todos los límites de la familia mientras que luchan para sobrevivir. " ,
-     horarios : [ " 13:50 " , " 16:20 " , " 19:20 " , " 22:10 " ]
-   },
-   {
-     Título :  " The Hurricane Heist " ,
-     Director :  " Rob Cohen " ,
-     estrellas : [ " Toby Kebbell " , " Maggie Grace " , " Ryan Kwanten " ],
-     imagen :  " https://images-na.ssl-images-amazon.com/images/M/MV5BMzg3Y2MyNjgtMzk4ZS00OTU3LWEwZmMtN2Y0NTdlZjU0NGFiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg " ,
-     Descripción :  " Los ladrones intenta un robo masivo contra el Tesoro de Estados Unidos como un huracán de categoría 5 se aproxima a una de sus instalaciones de menta. " ,
-     horarios : [ " 13:10 " , " 15:40 " , " 18:20 " , " 20:50 " , " 23:10 " ]
-   },
-   {
-     título :  " gringo " ,
-     director :  " Nash Edgerton " ,
-     estrellas : [ " Joel Edgerton " , " Charlize Theron " , " David Oyelowo " ],
-     imagen :  " https://images-na.ssl-images-amazon.com/images/M/MV5BMjAyMTk2MTQ3Ml5BMl5BanBnXkFtZTgwNDQ2ODE0NDM@._V1_UX182_CR0,0,182,268_AL_.jpg " ,
-     Descripción :  " GRINGO, una comedia negra mezclada con acción nudillos blancos y la intriga dramática, explora la batalla de supervivencia para los negocios Harold Soyinka (David Oyelowo) cuando se encuentra cruzando la línea de los ciudadanos respetuosos de la ley a criminal buscado. " ,
-     horarios : [ " 13:40 " , " 15:50 " , " 19:00 " , " 21:20 " , " 23:50 " ]
-   },
-   {
-     título :  " pura sangre " ,
-     director :  " Cory Finley " ,
-     estrellas : [ " Anya Taylor-Joy " , " Olivia Cooke " , " Anton Yelchin " ],
-     imagen :  " https://images-na.ssl-images-amazon.com/images/M/MV5BNDcyNDA4NDAzN15BMl5BanBnXkFtZTgwODQxMDQ5NDM@._V1_UX182_CR0,0,182,268_AL_.jpg " ,
-     Descripción :  " Dos adolescentes de clase alta en los suburbios de Connecticut reavivar su amistad poco después de años de crecimiento, aparte Juntos, traman un plan para resolver tanto de sus problemas, no importa cuál sea el costo.. " ,
-     horarios : [ " 13:00 " , " 15:30 " , " 18:00 " , " 20:10 " , " 22:40 " ]
-   },
-   {
-     Título :  " The Leisure Seeker " ,
-     Director :  " Paolo Virzì " ,
-     estrellas : [ " Helen Mirren " , " Donald Sutherland " , " Janel Moloney " ],
-     imagen :  " https://images-na.ssl-images-amazon.com/images/M/MV5BMTg1NTg2MzcyNF5BMl5BanBnXkFtZTgwNjMwMDIzNDM@._V1_UX182_CR0,0,182,268_AL_.jpg " ,
-     descripción :  " Una pareja fugitiva realiza un viaje inolvidable en el fiel y viejo RV que llaman The Leisure Seeker, que viaja desde Boston a The Ernest Hemingway Home en Key West. Recobran su pasión por la vida y su amor mutuo en un viaje por carretera que proporciona la revelación y la sorpresa hasta el final. " ,
-     horarios : [ " 13:40 " , " 15:50 " , " 19:00 " , " 21:20 " , " 23:50 " ]
-   },
-   {
-     título :  " pantera negra " ,
-     director :  " Ryan Coogler " ,
-     estrellas : [ " Chadwick Boseman " , " Michael B. Jordan " , " Lupita Nyong'o " ],
-     imagen :  " https://images-na.ssl-images-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@.V1_UX182_CR0,0,182.268_AL_.jpg " ,
-     Descripción :  " T'Challa, el rey de Wakanda, sube al trono en la aislada nación africana, tecnológicamente avanzada, pero su demanda es desafiado por un extraño vengativo que era una víctima de la infancia de error del padre de T'Challa. " ,
-     horarios : [ " 13:50 " , " 16:20 " , " 19:20 " , " 22:10 " ]
-   },
-   {
-     título :  " gorrión rojo " ,
-     Director :  " Francis Lawrence " ,
-     estrellas : [ " Jennifer Lawrence " , " Joel Edgerton " , " Matthias Schoenaerts " ],
-     imagen :  " https://images-na.ssl-images-amazon.com/images/M/MV5BMTA3MDkxOTc4NDdeQTJeQWpwZ15BbWU4MDAxNzgyNTQz._V1_UX182_CR0,0,182,268_AL_.jpg " ,
-     Descripción :  "bailarina Dominika Egorova es reclutado para 'Escuela Sparrow,' un servicio de inteligencia ruso, donde se ve obligada a utilizar su cuerpo como arma su primera misión, dirigida a un agente de la CIA, amenaza con deshacer la seguridad de ambas naciones. " ,
-     horarios : [ " 13:00 " , " 15:30 " , " 18:00 " , " 20:10 " , " 22:40 " ]
-   }
- ];
+'use strict'
+
+const movies = [
+  {
+    title: 'A Wrinkle in Time',
+    director: 'Ava DuVernay',
+    stars: ['Storm Reid', 'Oprah Winfrey', 'Reese Witherspoon'],
+    image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjMxNjQ5MTI3MV5BMl5BanBnXkFtZTgwMjQ2MTAyNDM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    description: "Following the discovery of a new form of space travel as well as Meg's father's disappearance, she, her brother, and her friend must join three magical beings - Mrs. Whatsit, Mrs. Who, and Mrs. Which - to travel across the universe to rescue him from a terrible evil.",
+    showtimes: ['13:00', '15:30', '18:00', '20:10', '22:40']
+  },
+  {
+    title: 'The Strangers: Prey at Night',
+    director: 'Johannes Roberts',
+    stars: ['Christina Hendricks', 'Bailee Madison', 'Martin Henderson'],
+    image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTY1OTIwODgzMV5BMl5BanBnXkFtZTgwMzUyMDgzNDM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    description: "A family's road trip takes a dangerous turn when they arrive at a secluded mobile home park to stay with some relatives and find it mysteriously deserted. Under the cover of darkness, three masked psychopaths pay them a visit to test the family's every limit as they struggle to survive.",
+    showtimes: ['13:50', '16:20', '19:20', '22:10']
+  },
+  {
+    title: 'The Hurricane Heist',
+    director: 'Rob Cohen',
+    stars: ['Toby Kebbell', 'Maggie Grace', 'Ryan Kwanten'],
+    image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzg3Y2MyNjgtMzk4ZS00OTU3LWEwZmMtN2Y0NTdlZjU0NGFiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    description: 'Thieves attempt a massive heist against the U.S. Treasury as a Category 5 hurricane approaches one of its Mint facilities.',
+    showtimes: ['13:10', '15:40', '18:20', '20:50', '23:10']
+  },
+  {
+    title: 'Gringo',
+    director: 'Nash Edgerton',
+    stars: ['Joel Edgerton', 'Charlize Theron', 'David Oyelowo'],
+    image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjAyMTk2MTQ3Ml5BMl5BanBnXkFtZTgwNDQ2ODE0NDM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    description: 'GRINGO, a dark comedy mixed with white-knuckle action and dramatic intrigue, explores the battle of survival for businessman Harold Soyinka (David Oyelowo) when he finds himself crossing the line from law-abiding citizen to wanted criminal.',
+    showtimes: ['13:40', '15:50', '19:00', '21:20', '23:50']
+  },
+  {
+    title: 'Thoroughbreds',
+    director: 'Cory Finley',
+    stars: ['Anya Taylor-Joy', 'Olivia Cooke', 'Anton Yelchin'],
+    image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BNDcyNDA4NDAzN15BMl5BanBnXkFtZTgwODQxMDQ5NDM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    description: 'Two upper-class teenage girls in suburban Connecticut rekindle their unlikely friendship after years of growing apart. Together, they hatch a plan to solve both of their problems-no matter what the cost.',
+    showtimes: ['13:00', '15:30', '18:00', '20:10', '22:40']
+  },
+  {
+    title: 'The Leisure Seeker',
+    director: 'Paolo Virzì',
+    stars: ['Helen Mirren', 'Donald Sutherland', 'Janel Moloney'],
+    image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTg1NTg2MzcyNF5BMl5BanBnXkFtZTgwNjMwMDIzNDM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    description: 'A runaway couple goes on an unforgettable journey in the faithful old RV they call The Leisure Seeker, traveling from Boston to The Ernest Hemingway Home in Key West. They recapture their passion for life and their love for each other on a road trip that provides revelation and surprise right up to the very end.',
+    showtimes: ['13:40', '15:50', '19:00', '21:20', '23:50']
+  },
+  {
+    title: 'Black Panther',
+    director: 'Ryan Coogler',
+    stars: ['Chadwick Boseman', 'Michael B. Jordan', "Lupita Nyong'o"],
+    image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    description: "T'Challa, the King of Wakanda, rises to the throne in the isolated, technologically advanced African nation, but his claim is challenged by a vengeful outsider who was a childhood victim of T'Challa's father's mistake.",
+    showtimes: ['13:50', '16:20', '19:20', '22:10']
+  },
+  {
+    title: 'Red Sparrow',
+    director: 'Francis Lawrence',
+    stars: ['Jennifer Lawrence', 'Joel Edgerton', 'Matthias Schoenaerts'],
+    image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTA3MDkxOTc4NDdeQTJeQWpwZ15BbWU4MDAxNzgyNTQz._V1_UX182_CR0,0,182,268_AL_.jpg',
+    description: "Ballerina Dominika Egorova is recruited to 'Sparrow School,' a Russian intelligence service where she is forced to use her body as a weapon. Her first mission, targeting a C.I.A. agent, threatens to unravel the security of both nations.",
+    showtimes: ['13:00', '15:30', '18:00', '20:10', '22:40']
+  }
+]
+
+module.exports = movies
